71e6b631beecf18cc4a5c6c1b3c080b27cdd3e81
rosemqa <ers6wobmo09m@mail.ru>
2025-01-20 22:57:06 +0300

    added more models to user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -1,12 +1,24 @@
-from pydantic.v1 import BaseModel
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class UserLoginModel(BaseModel):
     user_id: int
 
 
-class DeleteUserModel(BaseModel):
+class LoginHeadersModel(BaseModel):
+    token: str = Field(alias='x-csrf-token')
+
+
+class LoginCookiesModel(BaseModel):
+    auth_sid: str
+
+
+class SuccessModel(BaseModel):
     success: str
+
+
+class ErrorModel(BaseModel):
+    error: str
 
 
 class GetAuthUserModel(BaseModel):
@@ -18,6 +30,8 @@ class GetAuthUserModel(BaseModel):
 
 
 class GetUserModel(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
     username: str
 
 
